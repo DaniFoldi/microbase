855b561244a1b6fc68b45d23554e347328b1d749
DaniFoldi <dani@stefan.hu>
2022-03-14 14:50:34 +0000

    Prioritize BungeeVanishAPI over VanishAPI

--- src/main/java/com/danifoldi/microbase/depend/PremiumVanishDepend.java
+++ src/main/java/com/danifoldi/microbase/depend/PremiumVanishDepend.java
@@ -6,14 +6,14 @@ import com.danifoldi.microbase.util.ClassUtil;
 import java.util.UUID;
 
 public class PremiumVanishDepend {
-    public static final boolean vInstalled = ClassUtil.check("de.myzelyam.api.vanish.VanishAPI");
     public static final boolean bvInstalled = ClassUtil.check("de.myzelyam.api.vanish.BungeeVanishAPI");
+    public static final boolean vInstalled = ClassUtil.check("de.myzelyam.api.vanish.VanishAPI");
 
     public static boolean vanished(UUID uuid) {
-        if (vInstalled) {
-            return de.myzelyam.api.vanish.VanishAPI.getInvisiblePlayers().contains(uuid);
-        } else if (bvInstalled) {
+        if (bvInstalled) {
             return de.myzelyam.api.vanish.BungeeVanishAPI.getInvisiblePlayers().contains(uuid);
+        } else if (vInstalled) {
+            return de.myzelyam.api.vanish.VanishAPI.getInvisiblePlayers().contains(uuid);
         } else {
             Microbase.logger.warning("vanished checking requires PremiumVanish on your platform");
             return false;
